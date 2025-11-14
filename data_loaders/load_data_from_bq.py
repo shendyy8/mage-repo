b19920de0cbf0454585ae827e23f4e6ebc78b07a
shendyy8 <admin@admin.com>
2025-11-14 06:05:38 +0000

    add pipeline

---
+++ data_loaders/load_data_from_bq.py
@@ -0,0 +1,40 @@
+from mage_ai.settings.repo import get_repo_path
+from mage_ai.io.bigquery import BigQuery
+from mage_ai.io.config import ConfigFileLoader
+from os import path
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@data_loader
+def load_data_from_big_query(*args, **kwargs):
+    """
+    Template for loading data from a BigQuery warehouse.
+    Specify your configuration settings in 'io_config.yaml'.
+
+    Docs: https://docs.mage.ai/design/data-loading#bigquery
+    """
+    
+    # get sql path
+    sql_path = kwargs['sql_path']
+    execution_date = kwargs['execution_date']
+
+    with open(sql_path, "r") as sql_data:
+        query = sql_data.read()
+
+    query = query.format(execution_date=execution_date)
+
+    config_path = path.join(get_repo_path(), 'io_config.yaml')
+    config_profile = 'default'
+
+    return BigQuery.with_config(ConfigFileLoader(config_path, config_profile)).load(query)
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
--- test_folder/sample1.sql
+++ test_folder/sample1.sql
@@ -1,1 +1,3 @@
-SELECT 1;
+SELECT *
+FROM `bigquery-public-data.crypto_dogecoin.blocks`
+WHERE timestamp_month = DATE_TRUNC(DATE_SUB(EXTRACT(DATE FROM DATETIME('{execution_date}')), INTERVAL 11 YEAR), MONTH)
